55b951ea1744cc64580172166e7b543b7077349d
Esabrina77 <146923436+Esabrina77@users.noreply.github.com>
2024-01-07 23:58:08 +0100

    new modifications

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ func main() {
 	manager.PrintColorResult("purple", "server is running...")
 
 	fmt.Println("")
-	manager.PrintColorResult("yellow", "CLICK HERE to OPEN JUL PAGE--->")
+	manager.PrintColorResult("yellow", "CLICK HERE to OPEN  PAGE--->")
 	manager.PrintColorResult("blue", " http://localhost:8080/welcome \n")
 	fmt.Println("")
 	manager.PrintColorResult("green", "TO STOP THE SERVER , PRESS  'ctrl+C' ")
